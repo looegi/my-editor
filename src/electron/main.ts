2cde2aff6607dff2df9ff0f9c959074cdf1b1e08
looegi <lc1512@humboldt.edu>
2023-02-01 18:35:19 -0800

    Electron

---
+++ src/electron/main.ts
@@ -0,0 +1,14 @@
+import { app, BrowserWindow } from 'electron'
+
+function createWindow(): void {
+    const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+
+    });
+
+    mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(() => {createWindow();
+})
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const src = path.join(process.cwd(), 'src', 'electron')
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: path.join(src, 'main.ts'),
+    output: {
+        path: path.join(process.cwd(), 'dist', 'my-editor'),
+        filename: 'shell.js'
+    },
+    module: {
+        rules: [
+            {test: /\.ts$/,
+            loader: 'ts-loader',
+            options:{
+                configFile: path.join(src, 'tsconfig.json')
+            }
+        }
+        ]
+    }
+}
